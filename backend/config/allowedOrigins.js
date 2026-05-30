c8ec4c9fc387a08fdc723142bc36639f39e379c1
mohammed1819 <mtaha_cse2305a2@mgit.ac.in>
2026-05-30 14:03:40 +0530

    Add Render & Vercel URLs to CORS whitelist

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://examplessssded.com",
     "http://localhost:5173",
-    "https://feedback-collection-xi.vercel.app"
+    "https://feedback-collection-xi.vercel.app",
+    "https://feedbackcollection-g245.onrender.com"
 ]
 module.exports = allowedOrigins
